da680a84f1e45fea315ee6acc7f1d6472142e10e
fahasoavana <andriamanampisoatendry@gmail.com>
2024-01-11 12:48:43 +0100

    base 11-01-24

--- base-meuble/base.sql
+++ base-meuble/base.sql
@@ -33,3 +33,25 @@ insert into Volume(nom) values ('volume1');
 insert into Style(nom) values ('style1');
 insert into Style_Materiau(idStyle,idMateriau) values (1,1);
 insert into Formule_meuble(idCategorie,idStyle,idVolume,idMateriau,quantite) values (1,1,1,1,2);
+
+--11/01/24
+
+create table Achat_Materiau(
+    id serial PRIMARY KEY,
+    idMateriau int REFERENCES Materiau(id),
+    quantite int,
+    date_achat date
+);
+
+CREATE table Stock_Materiau(
+    id serial primary key,
+    id_Materiau int REFERENCES Materiau(id),
+    stock int,
+    date_stock int 
+);
+
+create table Fabrication(
+    id SERIAL primary key,
+    idFormule_Meuble int REFERENCES Formule_meuble(id),
+    quantite int
+);
